5b8b1af96a6041bb7e449993cbb922de9bd503bf
cblgh <cblgh@cblgh.org>
2021-03-18 15:48:51 +0100

    wip refactor nodejs muxrpc opening tunnel test

--- muxrpc/test/nodejs/testscripts/legacy_client_opening_tunnel.js
+++ muxrpc/test/nodejs/testscripts/legacy_client_opening_tunnel.js
@@ -1,71 +1,9 @@
-const pull = require('pull-stream')
-const { readFileSync } = require('fs')
-
-
-let newConnections = 0
+const secretStackPlugins = require('./secretstack-legacy')
+const before = require('./minimal-before-setup')
+const performOpeningTunnelTest = require('./client-opening-tunnel')
 
 module.exports = {
-    secretStackPlugins: ['ssb-conn', 'ssb-room/tunnel/client'],
-
-    before: (t, client, ready) => {
-        // nothing to prepare (like publishes messages, or...)
-        ready()
-    },
-
-    after: (t, client, roomrpc, exit) => {
-        newConnections++
-        t.comment('client new connection!' + roomrpc.id)
-        t.comment('total connections:' + newConnections)
-
-        if (newConnections > 1) {
-            t.comment('got a 2nd connection')
-            return
-        }
-        // now connected to the room server
-
-        // log all new endpoints
-        pull(
-            roomrpc.tunnel.endpoints(),
-            pull.drain(el => {
-                t.comment("from roomsrv:", el)
-            })
-        )
-
-        roomrpc.tunnel.isRoom((err, yes) => {
-            t.error(err, "tunnel.isRoom failed")
-            t.equal(yes, true, "expected isRoom to return true!")
-            
-            t.comment("peer is indeed a room!")
-
-            // announce ourselves to the room/tunnel
-            roomrpc.tunnel.announce().then((ret) => {
-                t.comment('announced!')
-
-                // put there by the go test process
-                let roomHandle = readFileSync('endpoint_through_room.txt').toString()
-                t.comment("connecting to room handle:" + roomHandle)
-
-                client.conn.connect(roomHandle, (err, tunneledrpc) => {
-                    t.error(err, "connected")
-                    t.comment("got tunnel to:", tunneledrpc.id)
-
-                    // check the tunnel connection works
-                    tunneledrpc.tunnel.ping((err, timestamp) => {
-                        t.error(err, "ping over the tunnel")
-                        t.true(timestamp > 0, "ping returns a timestamp")
-                        t.comment("ping:"+timestamp)
-
-                        roomrpc.tunnel.leave().then((ret) => {
-                            t.comment('left room... exiting in 1s')
-                            setTimeout(exit, 1000)
-                        }).catch((err) => {
-                            t.error(err, 'leave')
-                        })
-                    })
-                })
-            }).catch((err) => {
-                t.error(err, 'announce')
-            })
-        })
-    }
+    secretStackPlugins,
+    before,
+    after: performOpeningTunnelTest
 }
--- muxrpc/test/nodejs/testscripts/modern_client_opening_tunnel.js
+++ muxrpc/test/nodejs/testscripts/modern_client_opening_tunnel.js
@@ -1,69 +1,9 @@
-const pull = require('pull-stream')
-const { readFileSync } = require('fs')
-
-
-let newConnections = 0
+const secretStackPlugins = require('./secretstack-modern') // use modern tunnel
+const before = require('./minimal-before-setup')
+const performOpeningTunnelTest = require('./client-opening-tunnel')
 
 module.exports = {
-    secretStackPlugins: [
-        'ssb-conn',
-        'ssb-room-client',
-    ],
-
-    before: (t, client, ready) => {
-        // nothing to prepare (like publishes messages, or...)
-        ready()
-    },
-
-    after: (t, client, roomrpc, exit) => {
-        newConnections++
-        t.comment('client new connection!' + roomrpc.id)
-        t.comment('total connections:' + newConnections)
-
-        if (newConnections > 1) {
-            t.comment('after call 2 - not doing anything')
-            return
-        }
-        // now connected to the room server
-
-        // log all new endpoints
-        pull(
-            roomrpc.tunnel.endpoints(),
-            pull.drain(el => {
-                t.comment("from roomsrv:" + JSON.stringify(el))
-            })
-        )
-
-        // announce ourselves to the room/tunnel
-        roomrpc.tunnel.announce().then((ret) => {
-            t.comment('announced!')
-
-            // put there by the go test process
-            let roomHandle = readFileSync('endpoint_through_room.txt').toString()
-            t.comment("connecting to room handle:", roomHandle)
-
-            client.conn.connect(roomHandle, (err, tunneledrpc) => {
-                t.error(err, 'connect through room')
-                t.comment("got tunnel to:", tunneledrpc.id)
-
-                // check the tunnel connection works
-                tunneledrpc.testing.working((err, ok) => {
-                    t.error(err, 'testing.working didnt error')
-                    t.true(ok, 'testing.working is true')
-
-                    // start leaving after 2s
-                    setTimeout(() => {
-                        roomrpc.tunnel.leave().then((ret) => {
-                            t.comment('left room... exiting in 3s')
-                            setTimeout(exit, 3000)
-                        }).catch((err) => {
-                            t.error(err, 'left leaving')
-                        })
-                    }, 2000)
-                })
-            })
-        }).catch((err) => {
-            t.error(err, 'announce on server')
-        })
-    }
+    secretStackPlugins,
+    before,
+    after: performOpeningTunnelTest
 }
