544c05b2908441e6a4789c345583e6a2f993c1fb
Henry <111202+cryptix@users.noreply.github.com>
2021-03-26 14:49:04 +0100

    Apply suggestions from code review
    change variable declaration in login-events.js to const
    
    Co-authored-by: André Staltz <andre@staltz.com>

--- web/assets/login-events.js
+++ web/assets/login-events.js
@@ -1,9 +1,9 @@
 // get the challenge from out of the HTML
-let sc = document.querySelector("#challenge").dataset.sc
-var evtSource = new EventSource(`/withssb/events?sc=${sc}`);
+const sc = document.querySelector("#challenge").dataset.sc
+const evtSource = new EventSource(`/withssb/events?sc=${sc}`);
 
-var ping = document.querySelector('#ping');
-var failed = document.querySelector('#failed');
+const ping = document.querySelector('#ping');
+const failed = document.querySelector('#failed');
 
 evtSource.onerror = (e) => {
     failed.textContent = "Warning: The connection to the server was interupted."
