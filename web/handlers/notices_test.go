f9b5015aa97e4e9b96b22f436fc19741797049ef
Henry <cryptix@riseup.net>
2021-04-02 10:50:04 +0200

    remove cookie stuff from notices test

--- web/handlers/notices_test.go
+++ web/handlers/notices_test.go
@@ -2,14 +2,12 @@ package handlers
 
 import (
 	"net/http"
-	"net/http/cookiejar"
 	"net/url"
 	"testing"
 
 	"github.com/ssb-ngi-pointer/go-ssb-room/roomdb"
 	"github.com/ssb-ngi-pointer/go-ssb-room/web/router"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 // TestNoticeSmokeTest ensures the most basic notice serving is working
@@ -58,7 +56,7 @@ Hello world!
 // then we log in as an admin and see that the edit links are there.
 func TestNoticesEditButtonVisible(t *testing.T) {
 	ts := setup(t)
-	a, r := assert.New(t), require.New(t)
+	a := assert.New(t)
 
 	ts.AliasesDB.ResolveReturns(roomdb.Alias{}, roomdb.ErrNotFound)
 
@@ -82,10 +80,7 @@ func TestNoticesEditButtonVisible(t *testing.T) {
 
 	// start preparing the ~login dance~
 	// TODO: make this code reusable and share it with the login => /dashboard http:200 test
-	// cookiejar: a very cheap client session
 	// TODO: refactor login dance for re-use in testing / across tests
-	jar, err := cookiejar.New(nil)
-	r.NoError(err)
 
 	// when dealing with cookies we also need to have an Host and URL-Scheme
 	// for the jar to save and load them correctly
@@ -96,8 +91,6 @@ func TestNoticesEditButtonVisible(t *testing.T) {
 
 	csrfCookie := resp.Result().Cookies()
 	a.Len(csrfCookie, 1, "should have one cookie for CSRF protection validation")
-	t.Log(csrfCookie)
-	jar.SetCookies(formEndpoint, csrfCookie)
 
 	csrfTokenElem := doc.Find("input[type=hidden]")
 	a.Equal(1, csrfTokenElem.Length())
@@ -123,34 +116,12 @@ func TestNoticesEditButtonVisible(t *testing.T) {
 	postEndpoint := ts.URLTo(router.AuthFallbackFinalize)
 
 	// construct HTTP Header with Referer and Cookie
-	var csrfCookieHeader = http.Header(map[string][]string{})
-	csrfCookieHeader.Set("Referer", "https://localhost")
-	cs := jar.Cookies(postEndpoint)
-	r.Len(cs, 1, "expecting one cookie for csrf")
-	theCookie := cs[0].String()
-	a.NotEqual("", theCookie, "should have a new cookie")
-	csrfCookieHeader.Set("Cookie", theCookie)
-	ts.Client.SetHeaders(csrfCookieHeader)
+	var refererHeader = make(http.Header)
+	refererHeader.Set("Referer", "https://localhost")
+	ts.Client.SetHeaders(refererHeader)
 
 	resp = ts.Client.PostForm(postEndpoint, loginVals)
 	a.Equal(http.StatusSeeOther, resp.Code, "wrong HTTP status code for sign in")
-
-	sessionCookie := resp.Result().Cookies()
-	jar.SetCookies(postEndpoint, sessionCookie)
-
-	var sessionHeader = http.Header(map[string][]string{})
-	cs = jar.Cookies(noticeURL)
-	// TODO: why doesnt this return the csrf cookie?!
-	r.NotEqual(len(cs), 0, "expecting a cookie!")
-	for _, c := range cs {
-		theCookie := c.String()
-		a.NotEqual("", theCookie, "should have a new cookie")
-		sessionHeader.Add("Cookie", theCookie)
-	}
-
-	// update headers
-	ts.Client.ClearHeaders()
-	ts.Client.SetHeaders(sessionHeader)
 
 	cnt := ts.MembersDB.GetByIDCallCount()
 	// now we are logged in, anchor tag should be there
