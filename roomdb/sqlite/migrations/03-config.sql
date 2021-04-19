8d1f790f57bbcf907ba7f93e05bbdc9485056cc8
cblgh <cblgh@cblgh.org>
2021-04-19 16:32:01 +0200

    add flag for toggling usage of alias subdomains
    related to #160, requested by cryptix

--- roomdb/sqlite/migrations/03-config.sql
+++ roomdb/sqlite/migrations/03-config.sql
@@ -4,6 +4,7 @@ CREATE TABLE config (
     id integer NOT NULL PRIMARY KEY,
     privacyMode integer NOT NULL,    -- open, community, restricted
     defaultLanguage TEXT NOT NULL,   -- a language tag, e.g. en, sv, de
+    use_subdomain_for_aliases boolean NOT NULL, -- flag to toggle using subdomains (rather than alias routes) for aliases
 
     CHECK (id == 0) -- should only ever store one row
 );
@@ -11,10 +12,11 @@ CREATE TABLE config (
 -- the config table will only ever contain one row: the rooms current settings
 -- we update that row whenever the config changes.
 -- to have something to update, we insert the first and only row at id 0
-INSERT INTO config (id, privacyMode, defaultLanguage) VALUES (
+INSERT INTO config (id, privacyMode, defaultLanguage, use_subdomain_for_aliases) VALUES (
     0,     -- the constant id we will query
     2,     -- community is the default mode unless overridden
-    "en"   -- english is the default language for all installs
+    "en",  -- english is the default language for all installs
+    1      -- use subdomain for aliases
 );
 
 -- +migrate Down
